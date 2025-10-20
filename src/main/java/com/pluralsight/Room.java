98aacebea826d92f4ff7a677bbb04690a66059f1
Nachi4265 <hvirgen@my.yearupunited.org>
2025-10-20 14:23:12 -0400

    commit

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -16,6 +16,7 @@ public class Room {
                 ", price=" + price +
                 ", isOccupied=" + isOccupied +
                 ", isDirty=" + isDirty +
+                ", isAvailable=" + isAvailable()+
                 '}';
     }
 
