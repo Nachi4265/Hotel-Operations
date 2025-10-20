2fd37a46973f6c45143c95bfe874dc0a59d146b0
Nachi4265 <hvirgen@my.yearupunited.org>
2025-10-20 13:36:54 -0400

    made rooms collection and made rooms

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,30 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        //making collection of rooms
+        ArrayList<Room>rooms = new ArrayList<>();
 
-       
+
+        //rooms with number of beds
+        Room r101 = new Room(1);
+        Room r102 = new Room(2);
+        Room r103 = new Room(2);
+        Room r104 = new Room(2);
+
+        //add room(r101) to our room collection
+        rooms.add(r101);
+        rooms.add(r102);
+        rooms.add(r103);
+        rooms.add(r104);
+
+        //iterate through our collection and tell us information about the room
+        for(Room r : rooms){
+            System.out.println(r);
+        }
 
 
     }
